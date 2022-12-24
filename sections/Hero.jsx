5f9217f5497e1be7ece3bfbe19fd83787e81a06b
I.Chandana <indu.99.ic@gmail.com>
2022-12-24 15:18:59 +0530

    Dec24-001-hero section

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -1,8 +1,39 @@
-'use client';
+"use client";
+
+import { motion } from "framer-motion";
+
+import styles from "../styles";
+import { slideIn, staggerContainer, textVariant } from "../utils/motion";
 
 const Hero = () => (
-  <section>
-    Hero section
+  <section className={`${styles.yPaddings} sm:pl-16 pl-6`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }} // when u |once:true| It will be render only once. when |once:false| It will be render every time.
+      className={`${styles.innerWidth} mx-auto flex flex-col`}
+    >
+      <div className="flex justify-center items-center flex-col relative z-10">
+        <motion.h1 variants={textVariant(1.1)} className={styles.heroHeading}>
+          Metaverse
+        </motion.h1>
+        <motion.div
+          variants={textVariant(1.2)} // 1.2 sec It's render just a bit after this h1
+          className="flex flex-row justify-center items-center"
+        >
+          <h1 className={styles.heroHeading}>Ma</h1>
+          <div className={styles.heroDText} />
+          <h1 className={styles.heroHeading}>Ness</h1>
+        </motion.div>
+      </div>
+
+      <motion.div variants={slideIn("right", "tween", 0.2, 1)} className="">
+        <div>
+          <img src="/cover.png" alt="cover" className="" />
+        </div>
+      </motion.div>
+    </motion.div>
   </section>
 );
 
